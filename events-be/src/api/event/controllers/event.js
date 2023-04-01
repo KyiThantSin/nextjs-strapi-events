fb84414956d74228b4a3cbc5d9b4fbf183bd892b
KyiThantSin <kyits485@gmail.com>
2023-04-01 16:18:29 +0630

    custom endpoint

--- events-be/src/api/event/controllers/event.js
+++ events-be/src/api/event/controllers/event.js
@@ -1,9 +1,33 @@
-'use strict';
+"use strict";
 
 /**
  * event controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Get logged in users
+  async me(ctx) {
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { message: "No authorization header was found" },
+      ]);
+    }
+
+    const data = await strapi.db.query("api::event.event").findMany({
+      where: {
+        user: { id: user.id },
+      },
+      populate: { user: true, image: true },
+    });
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    const res = await this.sanitizeOutput(data, ctx);
+    return res;
+  },
+}));
